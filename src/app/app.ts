6ae9db7a94107ac58cc670296ebc52c35001be24
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-27 09:55:29 +0530

    Completed Loops and else if Statement Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,43 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-part4';
+  // color: number = 1;
+
+  // colorcall(val: number) {
+  //   this.color = val;
+  //   console.log(this.color);
+  // }
+
+  // handleInput(event: Event) {
+  //   this.color = parseInt((event.target as HTMLInputElement).value);
+  // }
+
+  // color = 'blue';
+
+  // handlecolor(val: string) {
+  //   this.color = val;
+  // }
+
+  // changecolor(event:Event) {
+  //   this.color=(event.target as HTMLInputElement).value;
+  // }
+
+  users = ['aman', 'suman', 'raman', 'shyam', 'gopal'];
+
+  students = [
+    { name: 'Aman', age: 20, city: 'Delhi' },
+    { name: 'Suman', age: 22, city: 'Mumbai' },
+    { name: 'Raman', age: 21, city: 'Kolkata' },
+    { name: 'Shyam', age: 23, city: 'Chennai' },
+    { name: 'Gopal', age: 24, city: 'Bangalore' },
+  ];
+
+  getName(name: string) {
+    console.log(name);
+    return name;
+  }
 }
